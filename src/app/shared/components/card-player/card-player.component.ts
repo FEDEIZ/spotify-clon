5548eed7fbaae37a0e4a09aa90fbfdc44fae435d
Interlandi <federico.interlandi@accenture.com>
2024-10-24 16:32:52 -0300

    section 6 allmost done

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { MultimediaService } from '@shared/services/multimedia.service';
 
 @Component({
   selector: 'app-card-player',
@@ -16,10 +17,14 @@ export class CardPlayerComponent implements OnInit{
     _id: 0
   };
 
-  constructor(){}
+  constructor(private multimediaService: MultimediaService){}
 
   ngOnInit(): void {
 
+  }
+
+  sendPlay(track:TrackModel): void{
+    this.multimediaService.callback.emit(track);
   }
 
 }
--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { MultimediaService } from '@shared/services/multimedia.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrl: './media-player.component.css'
 })
-export class MediaPlayerComponent {
+export class MediaPlayerComponent implements OnInit, OnDestroy {
+  listObservers$: Subscription[] = [];
   mockCover: TrackModel = {
     cover: 'https://i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
     name: 'Gioli & Assia',
     album: 'BEBE (Oficial)',
     url: '',
     _id: 1
+  }
+
+  constructor(private multimediaService: MultimediaService){}
+
+  ngOnInit(): void {
+    const observer1$: Subscription = this.multimediaService.callback.subscribe(
+      (response:TrackModel) => {
+
+      }
+    )
+    this.listObservers$ = [observer1$]
+  }
+
+  ngOnDestroy(): void {
+    this.listObservers$.forEach(u => u.unsubscribe());
   }
 }
